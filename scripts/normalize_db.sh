f3792960ab18b74c9638dac9ba98af2495dbb1ee
aaaaluki <luki00.lrb@gmail.com>
2022-10-04 15:38:45 +0200

    Scripts: The output sample rate is fixed and the meta files are copied

--- scripts/normalize_db.sh
+++ scripts/normalize_db.sh
@@ -8,6 +8,7 @@
 # Global variables
 DATABASE_FOLDER=""
 OUTPUT_FOLDER=""
+OUTPUT_SAMPLE_RATE=22050
 
 function _usage() {
     echo "Usage: $0 <database_folder>"
@@ -37,15 +38,21 @@ function main() {
     _info_msg "Procesing WAVs from \"${DATABASE_FOLDER}\" into \"${OUTPUT_FOLDER}\""
     database_files=$(ls $DATABASE_FOLDER/*.wav)
     database_size=$(ls $DATABASE_FOLDER/*.wav | wc -l)
+    meta_files=$(ls $DATABASE_FOLDER | grep -vE "\.wav$")
 
+    # Copy the meta files (metadata, license, others...) to the created folder
+    for mfile in $meta_files; do
+        cp -r "${DATABASE_FOLDER}/${mfile}" "${OUTPUT_FOLDER}/${mfile}"
+    done
+
+    # So some processing on the database files
     i=0
     for file in $database_files
     do
-        # So some processing on the input file
         out_file="${OUTPUT_FOLDER}/$(basename $file)"
 
-        # Duplicate left channel to L and R
-        sox $file $out_file remix 1 1
+        # Resample file to 22050 Hz and mix all input channels
+        sox $file -r $OUTPUT_SAMPLE_RATE $out_file remix -
 
         _progress $i $database_size
         i=$(( i+=1 ))
