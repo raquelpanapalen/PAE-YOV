ae5692f30cc622273259ad194de290dd9c29f32d
aaaaluki <luki00.lrb@gmail.com>
2022-10-16 01:05:54 +0200

    Scripts/add_points.py: using function args instead of global args

--- scripts/add_points.py
+++ scripts/add_points.py
@@ -11,17 +11,17 @@ SPLIT_CHAR = '|'
 LEGAL_ENDINGS = '.,;'
 
 
-def process_file(input_path: str, output_path: str) -> None:
+def process_file(input_path: str, output_path: str, split_char: str, legal_endings: str) -> None:
     with open(input_path, 'r') as f_in, open(output_path, 'w') as f_out:
         info(f'Reading from "{input_path}" and writing to "{output_path}"')
         for line in f_in.readlines():
-            parts = line.strip().split(SPLIT_CHAR)
+            parts = line.strip().split(split_char)
             out_line = parts[0]
             for p in parts[1:]:
-                if p[-1] not in LEGAL_ENDINGS:
-                    p += LEGAL_ENDINGS[0]
+                if p[-1] not in legal_endings:
+                    p += legal_endings[0]
 
-                out_line += SPLIT_CHAR + p
+                out_line += split_char + p
 
             f_out.write(out_line + '\n')
 
@@ -36,15 +36,14 @@ def main(args: Type[argparse.Namespace]):
     if os.path.exists(output_path):
         warn('Output file exists, overwriting!')
 
-    SPLIT_CHAR = args.split_char
-    process_file(input_path, output_path)
+    process_file(input_path, output_path, args.split_char, LEGAL_ENDINGS)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('INPUT', type=str, help='Input file')
     parser.add_argument('OUTPUT', type=str, help='Output file')
-    parser.add_argument('-s', '--split_char', type=str, help='Delimiter char on the input/output files')
+    parser.add_argument('-s', '--split_char', type=str, help='Delimiter char on the input/output files', default=SPLIT_CHAR)
     args = parser.parse_args()
 
     main(args)
